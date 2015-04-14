b0c60769a2300b2993093f0c88de1130810b561a
ken <kenken17@gmail.com>
2015-04-14 10:27:04 +0800

    fix the issue on generic should/not helper. Check param to make sure it only have 2 argument instead.

--- client/helpers_class.js
+++ client/helpers_class.js
@@ -1,19 +1,17 @@
 MUtilities = MUtilities || {};
 
 UI.registerHelper('should', function(verb, x, y) {
-	if (y) {
-		return x === y ? verb : '';
-	} else {
+	if (arguments.length <= 2) {
 		return x ? verb : '';
+	} else {
+		return x === y ? verb : '';
 	}
-
-	return x === y ? verb : '';
 });
 
 UI.registerHelper('not', function(verb, x, y) {
-	if (y) {
-		return x === y ? '': verb;
-	} else {
+	if (arguments.length <= 2) {
 		return x ? '' : verb;
+	} else {
+		return x === y ? '': verb;
 	}
 });
