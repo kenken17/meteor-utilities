0f28e6c2a248861c792d6fb0917625c7ee2970d6
ken <kenken17@gmail.com>
2015-04-16 12:25:33 +0800

    check for invalid date

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
 	name: 'kenken:meteor-utilities',
 	summary: 'A collection of common utilities for general app development.',
-	version: '2.1.2',
+	version: '2.1.3',
 	git: 'https://github.com/kenken17/meteor-utilities'
 });
 
