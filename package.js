19923b297ad1f9e04e7fd6b4444597aa59f6d0ce
ken <kenken17@gmail.com>
2015-04-21 17:24:08 +0800

    Bump to version 2.3.4

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
 	name: 'kenken:meteor-utilities',
 	summary: 'A collection of common utilities for general app development.',
-	version: '2.3.3',
+	version: '2.3.4',
 	git: 'https://github.com/kenken17/meteor-utilities'
 });
 
